90b222b71c0fb5028570c1827b8167baf09687e0
musa1992 <mosesdev92@gmail.com>
2023-02-24 19:04:58 +0300

    add style to the card in main gallery

--- client/src/components/gallery/Card.js
+++ client/src/components/gallery/Card.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import ImageSlider from '../slider/ImageSlider.js'
-
 import img_one from '../../images/img-01.webp'
 import img_two from '../../images/img-02.webp'
 import img_three from '../../images/img-03.webp'
@@ -19,11 +20,15 @@ const Card = props => {
 	];
 	return (
 		<div className='card'>
-			<ImageSlider imgContainer={imgContainer} type={"pure"}/>
+			<FontAwesomeIcon icon={faHeart} className='crd-save-icon'/>
+			<div className='card-slider'>
+			  <ImageSlider imgContainer={imgContainer} type={"pure"}/>
+			</div>
 			<div className='card-info'>
-				<p className='text-300'>Clay City, Kasarani</p>
-				<p className='text-light text-300'>Nairobi</p>
-				<p className='text-bold-500 price'>Ksh. 22,000</p>
+			  <p className='text-300'>Clay City, Kasarani</p>
+			  <p className='text-light text-300'>Nairobi</p>
+			  <p className='text-300'>2 bedroom | 1 bath</p>
+			  <p className='text-bold-500 price'>Ksh. 22,000</p>
 			</div>
 		</div>
 	)
