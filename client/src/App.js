5cfc4c7ccd5efa15afbe258e50713a675da3b963
musa1992 <mosesdev92@gmail.com>
2023-02-24 16:51:25 +0300

    add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Header from './components/header/Header';
 import Gallery from './components/gallery/Gallery';
-import ListingPage from './components/listing/ListingPage';
+import ListingPage from './components/listing/ListingPage.js';
 function App() {
   return (
-    <div>
-      <Header/>
-      <Gallery/>
-      <ListingPage/>
-    </div>
+    <Router>
+      <div>
+	  <Header/>
+	  <Routes>
+	  	<Route path='/' element={<Gallery/>}/>
+	  	<Route path='/listing' element={<ListingPage/>}/>
+	  </Routes>
+      </div>
+    </Router>	  
   );
 }
 
