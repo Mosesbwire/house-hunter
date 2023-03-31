657b274bef6eb3fc4f62a0b8877039047f75d0eb
musa1992 <mosesdev92@gmail.com>
2023-03-31 12:33:20 +0300

    Test class

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom'
 import Header from '../header/Header'
 const Layout = () => {
   return (
-    <>
+    <div>
       <Header/>
       <Outlet/>
-    </>
+    </div>
   )
 }
 
