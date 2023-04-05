b743afaccdd3cb220925f9939ed153cf02cee56d
musa1992 <mosesdev92@gmail.com>
2023-04-06 00:04:09 +0300

    add drop down for search locations

--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Search = props => {
+  const [openSearch, setOpenSearch] = useState(false)
+  const toggleSearchLocation = ()=> {
+    setOpenSearch(!openSearch)
+  }
   return (
     <div className='search-bar'>
       <input type='search' placeholder='Start your search' className='search-input'/>
-      <div className='search-icon'>
+      <div className='search-icon' onClick={toggleSearchLocation}>
         <FontAwesomeIcon icon={faMagnifyingGlass} className='magnifying-glass'/>
       </div>
+      {openSearch ? <div className='search-locations'>
+        <div className='locations-wrapper'>
+          <ul className='location-items'>
+            <li>Kasarani</li>
+            <li>Kahawa West</li>
+            <li>Ruaka</li>
+            <li>Umoja</li>
+            <li>Parklands</li>
+            <li>Westlands</li>
+            <li>Donholm</li>
+            <li>Utawala</li>
+          </ul>
+        </div>
+      </div> : null }
+      
     </div>
   )
 }
