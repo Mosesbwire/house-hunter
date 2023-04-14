a15711943387477c84a9ae99c04acae03f6e2bd1
musa1992 <mosesdev92@gmail.com>
2023-04-14 23:54:02 +0300

    add customer router to middleware chain in App.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const connectDB = require('../config/db.config')
+const { customerApi } = require('./components/customers/index')
 
 var app = express();
 connectDB()
@@ -11,6 +12,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/api/v1/customers', customerApi)
 
 
 module.exports = app;
