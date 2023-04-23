d7f3bca85d2dec91258e44c247b202a0262d6050
musa1992 <mosesdev92@gmail.com>
2023-04-23 16:36:55 +0300

    add listing router to app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const connectDB = require('../config/db.config')
 const { customerApi } = require('./components/customers/index')
+const { listingApi } = require("./components/listings/index")
 
 var app = express();
 connectDB()
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api/v1/customers', customerApi)
+app.use('/api/v1/listings', listingApi)
 
 
 module.exports = app;
