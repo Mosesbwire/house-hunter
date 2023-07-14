d555bae390b7d1cddb75700193a608d95be37080
musa1992 <mosesdev92@gmail.com>
2023-07-14 19:16:37 +0300

    Add req to returnError. Check if error is type NotFoundError - respond with custom error

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const returnError = require('../utils/error/returnError')
 function errorHandler(error, req, res, next){
-    returnError(error, res)
+    returnError(error, req, res)
 }
 
 module.exports = errorHandler
--- src/utils/error/returnError.js
+++ src/utils/error/returnError.js
@@ -1,6 +1,8 @@
-function returnError(err, res){
+function returnError(err, req, res){
     const statusCode = err.statusCode || 500;
-
+    if (err.name === 'NotFoundError'){
+        err.message = `${err.message} at specified path ${req.path} not found`
+    }
     res.status(statusCode).json({
         statusCode,
         message: err.message,
