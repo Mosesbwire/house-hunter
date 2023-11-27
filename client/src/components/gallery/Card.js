b6f2dc3cfc5e08060bee6f392a99c7ce0678267c
Mosesbwire <mosesdev92@gmail.com>
2023-11-28 00:24:43 +0300

    add indicator to show which listings have already been liked

--- client/src/components/gallery/Card.js
+++ client/src/components/gallery/Card.js
@@ -1,18 +1,21 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { toast } from 'react-toastify'
 import PropTypes from 'prop-types'
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
 import ImageSlider from '../slider/ImageSlider.js'
 import { authContext } from '../../context/authContextProvider.js'
 import { ListingContext } from '../../context/listingsContextProvider.js'
 import { likeListing } from '../../actions/listing.js'
+import { getItemFromSessionStorage, saveToSessionStorage } from '../../utils/utils.js'
 
 const Card = props => {
 	const navigate = useNavigate()
 	const ctx = useContext(authContext) 
 	const listingCtx = useContext(ListingContext)
+	const [isLiked, setIsLiked] = useState(false);
 	const handleClick = (e)=>{
 		navigate(`/${props.listing.details.buildingType}/${props.listing.location.mainLocation}/${props.listing.id}`)
 	}
@@ -27,17 +30,32 @@ const Card = props => {
 		} else {
 			await likeListing(listingCtx, props.listing.id)
 			if (!listingCtx.state.loading) {
-				console.log('Liked');
+				const user = getItemFromSessionStorage("user");
+				const likes = user.savedListings
+				if (!likes.includes(props.listing.id)){
+					user.savedListings.push(props.listing.id)
+				} else {
+					const index = user.savedListings.indexOf(props.listing.id)
+					user.savedListings.splice(index, 1)
+
+				}
+				saveToSessionStorage('user', user)
+				setIsLiked(!isLiked)
+			}
+		}	
+	}
+	useEffect(() => {
+		if (ctx.state.isAuthenticated) {
+			const user = getItemFromSessionStorage("user");
+			const likes = user.savedListings
+			if (likes.includes(props.listing.id)) {
+				setIsLiked(true)
 			}
 		}
-		
-		
-	}
-	
+	}, [ctx.state.isAuthenticated, props.listing.id])
 	return (
 		<div className='card' onClick={(e) => handleClick(e)}>
-
-			<FontAwesomeIcon icon={faHeart} className='crd-save-icon'onClick={e => saveListing(e)}/>
+			<FontAwesomeIcon icon={isLiked ? faHeartSolid : faHeart} className='crd-save-icon'onClick={e => saveListing(e)}/>
 			<div className='card-slider'>
 			  <ImageSlider imgContainer={props.listing.imageUrls} type={"pure"}/>
 			</div>
