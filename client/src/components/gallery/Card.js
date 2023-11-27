a74b8f73eac2188d57cc4f166a9644f0fdeb38e9
Mosesbwire <mosesdev92@gmail.com>
2023-11-27 22:39:22 +0300

    show toast if like button is clicked by unauthenticated user

--- client/src/components/gallery/Card.js
+++ client/src/components/gallery/Card.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { toast } from 'react-toastify'
 import PropTypes from 'prop-types'
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import ImageSlider from '../slider/ImageSlider.js'
+import { authContext } from '../../context/authContextProvider.js'
+import { ListingContext } from '../../context/listingsContextProvider.js'
+import { likeListing } from '../../actions/listing.js'
 
 const Card = props => {
 	const navigate = useNavigate()
+	const ctx = useContext(authContext) 
+	const listingCtx = useContext(ListingContext)
 	const handleClick = (e)=>{
 		navigate(`/${props.listing.details.buildingType}/${props.listing.location.mainLocation}/${props.listing.id}`)
 	}
+
+	const saveListing = async (e) => {
+		e.stopPropagation()
+		if (!ctx.state.isAuthenticated) {
+			toast.info('Login to save your likes', {
+				position: toast.POSITION.TOP_CENTER,
+				autoClose: false
+			})
+		} else {
+			await likeListing(listingCtx, props.listing.id)
+			if (!listingCtx.state.loading) {
+				console.log('Liked');
+			}
+		}
+		
+		
+	}
+	
 	return (
 		<div className='card' onClick={(e) => handleClick(e)}>
 
-			<FontAwesomeIcon icon={faHeart} className='crd-save-icon'/>
+			<FontAwesomeIcon icon={faHeart} className='crd-save-icon'onClick={e => saveListing(e)}/>
 			<div className='card-slider'>
 			  <ImageSlider imgContainer={props.listing.imageUrls} type={"pure"}/>
 			</div>
